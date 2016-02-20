912e08b327b6f788bf511ffbaa8eac094380739b
Zac <zschreck@iastate.edu>
2016-02-20 14:17:30 -0600

    changed layout a little

--- bundles1.js
+++ bundles1.js
@@ -1,12 +1,11 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})
 ({1:[function(require,module,exports){
-/**
- * 
- */// fill in your key and secret 
- // you can get them from http://developer.factual.com/display/docs/Factual+Developer+APIs+Version+3 
+
  var KEY = 'a9DtHbJ5UyLUua4umVtSAgQzwpd1ub8aeEBr2zhg'; 
 
  var SECRET = 'sQbsWMHDMdhz9rMMlkJ3OtcoUnx91TuXiG5dDIKg'; 
+ 
+
  
  module.exports = { 
    key: KEY, 
@@ -14,3 +13,28 @@
  }; 
 
 },{}]},{},[1]);
+
+
+var Factual = require('factual-api');
+var YOUR_KEY = "a9DtHbJ5UyLUua4umVtSAgQzwpd1ub8aeEBr2zhg";
+var YOUR_SECRET = 'sQbsWMHDMdhz9rMMlkJ3OtcoUnx91TuXiG5dDIKg';
+var ZIPCODE = '50014';
+var factual = new Factual(YOUR_KEY, YOUR_SECRET);
+
+factual.get('/t/places-us/schema', function(error, res) {
+	console.log(res.view);
+});
+
+//https:// www.factual.com/data/t/restaurants-us#filters={"$and":[{"postcode":{"$search":"50014"}}]}
+factual.get('/t/restaurants-us?', {
+	filters : {
+		"$and" : [ {
+			"postcode" : {
+				"$search" : ZIPCODE
+			}
+		} ]
+	},
+	select : "contextname,factual_id"
+}, function(error, res) {
+	console.log(res.data);
+});

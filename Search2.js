77c1d375b4a7c7308795fa1316c5ea945c382b54
Zac <panzerzac15@gmail.com>
2016-02-20 15:04:12 -0600

    Delete Search2.js

--- Search2.js
+++
@@ -1,7 +0,0 @@
-$("#links").load("/Main_Page #jq-p-Getting-Started li");
-var out = ''; // container for fetched values
-console.log("made it 1\n");
-var input = '50014';
-
-// var url = 'https://www.factual.com/data/t/restaurants-us#filters={"$and":[{"postcode":{"$search":"' + input + '"}}]}';
-

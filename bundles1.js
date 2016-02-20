da183c848bc2410a49fa2528294804769f9305b4
Zac <zschreck@iastate.edu>
2016-02-20 03:50:25 -0600

    pushing

---
+++ bundles1.js
@@ -0,0 +1,16 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})
+({1:[function(require,module,exports){
+/**
+ * 
+ */// fill in your key and secret 
+ // you can get them from http://developer.factual.com/display/docs/Factual+Developer+APIs+Version+3 
+ var KEY = 'a9DtHbJ5UyLUua4umVtSAgQzwpd1ub8aeEBr2zhg'; 
+
+ var SECRET = 'sQbsWMHDMdhz9rMMlkJ3OtcoUnx91TuXiG5dDIKg'; 
+ 
+ module.exports = { 
+   key: KEY, 
+   secret: SECRET 
+ }; 
+
+},{}]},{},[1]);
